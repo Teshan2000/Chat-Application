96f520f08f29bd400abf60a0ef26ac3d9894293a
Teshan2000 <teshan_wijewardhana@outlook.com>
2024-05-12 22:55:30 +0530

    Chat Interface
    Interface of chat application was designed.

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,100 +1,67 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
+?>
+
+
+<!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
-
-	<style type="text/css">
-
-	::selection { background-color: #E13300; color: white; }
-	::-moz-selection { background-color: #E13300; color: white; }
-
-	body {
-		background-color: #fff;
-		margin: 40px;
-		font: 13px/20px normal Helvetica, Arial, sans-serif;
-		color: #4F5155;
-	}
-
-	a {
-		color: #003399;
-		background-color: transparent;
-		font-weight: normal;
-		text-decoration: none;
-	}
-
-	a:hover {
-		color: #97310e;
-	}
-
-	h1 {
-		color: #444;
-		background-color: transparent;
-		border-bottom: 1px solid #D0D0D0;
-		font-size: 19px;
-		font-weight: normal;
-		margin: 0 0 14px 0;
-		padding: 14px 15px 10px 15px;
-	}
-
-	code {
-		font-family: Consolas, Monaco, Courier New, Courier, monospace;
-		font-size: 12px;
-		background-color: #f9f9f9;
-		border: 1px solid #D0D0D0;
-		color: #002166;
-		display: block;
-		margin: 14px 0 14px 0;
-		padding: 12px 10px 12px 10px;
-	}
-
-	#body {
-		margin: 0 15px 0 15px;
-		min-height: 96px;
-	}
-
-	p {
-		margin: 0 0 10px;
-		padding:0;
-	}
-
-	p.footer {
-		text-align: right;
-		font-size: 11px;
-		border-top: 1px solid #D0D0D0;
-		line-height: 32px;
-		padding: 0 10px 0 10px;
-		margin: 20px 0 0 0;
-	}
-
-	#container {
-		margin: 10px;
-		border: 1px solid #D0D0D0;
-		box-shadow: 0 0 8px #D0D0D0;
-	}
-	</style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="styles/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&family=Oswald:wght@500;600;700&family=Pacifico&display=swap" rel="stylesheet"> 
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+    <title>Document</title>
 </head>
 <body>
+    <div class="container-fluid bg-primary p-3">
+        <div class="row">
+            <div class="col-lg-1 text-center"></div>
+            <div class="col-lg-10 pt-2 text-center">
+                <h3 class="text-uppercase text-light">Chat Application</h3>
+            </div>
+            <div class="col-lg-1 text-center"><a href="login.php">
+                <i class="fa fa-user fs-3"></i><br>
+                <span class="text-light">Login</span></a>
+            </div>
+        </div>
+    </div>
 
-<div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
-
-	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
-
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
-
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/Welcome.php</code>
-
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="userguide3/">User Guide</a>.</p>
-	</div>
-
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
-</div>
-
+    <div class="container">
+        <div class="row pt-5">
+            <div class="col-lg-10 mx-auto">
+                <div class="card">
+                    <div class="card-header bg-primary text-center text-white pt-3">
+                        <h5>Teshan Wijewardhana</h5>
+                    </div>
+                    <div class="card-body">
+                        <!-- <h5 class="card-title text-center">Teshan Wijewardhana</h5> -->
+                        <div class="row" style="float: right; width:25%;">
+                            <div class="pb-1">                
+                                <div class="card shadow-sm bg-white rounded mb-4 p-2">
+                                    <div class="flex-fill pl-3">
+                                        <h6 class="text-start">Hello!</h6>   
+                                        <small class="text-end">Just Now</small>                            
+                                    </div>
+                                </div>
+                            </div> 
+                        </div>
+                        <div class="row" style="float: right; width:102%;">
+                            <form action="" method="post" enctype="multipart/form-data">
+                                <div class="form-group pb-3 d-flex">
+                                    <input type="text" class="form-control pe-2" name="message" id="message" placeholder="Type message..."/>
+                                    <button type="submit" class="btn btn-primary ps-2 ms-2" name="submit" style="border-radius: 10%;"><i class="fa fa-paper-plane"></i></button>
+                                </div>  
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+                
 </body>
 </html>
